38ce8eecba80f2016508bf94f301612587b64651
Y1lmaz-Ahmet <y.ahmet@outlook.be>
2024-06-23 21:57:40 +0200

    update about.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -28,22 +28,23 @@ const About = () => {
         <h2>Mijn Verhaal</h2>
         <p>
           Mijn reis begon in Lokeren op 7 december 1998 en bracht me naar Zele,
-          België. Mijn fascinatie voor technologie begon met mijn eerste
-          PlayStation 2, vooral door het spel Ratchet & Clank. De
-          nieuwsgierigheid naar hoe deze karakters bewogen en animeerden leidde
-          tot mijn interesse in programmeren.
+          België. Mijn fascinatie voor technologie begon al op jonge leeftijd
+          toen ik voor het eerst in aanraking kwam met computers en digitale
+          interacties. De nieuwsgierigheid naar hoe interfaces werkten en hoe
+          animaties tot stand kwamen, leidde tot mijn interesse in programmeren.
         </p>
         <p>
-          Op 9-jarige leeftijd begon ik te gamen op de computer van mijn broer,
-          met name RuneScape, waar ik ook Engels leerde. Deze vroege ervaringen
-          hebben mijn probleemoplossende vaardigheden aangescherpt, een cruciaal
-          aspect in mijn latere programmeercarrière.
+          Op 9-jarige leeftijd begon ik te experimenteren met computers, wat
+          mijn probleemoplossende vaardigheden aanscherpte. Deze vroege
+          ervaringen waren cruciaal in mijn latere programmeercarrière en
+          versterkten mijn interesse in het bouwen van gebruiksvriendelijke
+          interfaces.
         </p>
         <p>
           In het secundair onderwijs volgde ik Elektrische Installatietechnieken
-          aan VTI Dendermonde en ontdekte mijn talent voor het programmeren van
-          PLCs. Na mijn afstuderen in Industriële Onderhoudstechnieken aan
-          Sint-Jan Berchmansinstituut vzw, vervolgde ik mijn droom aan de
+          aan VTI Dendermonde, waar ik mijn talent voor het programmeren van
+          PLCs ontdekte. Na mijn afstuderen in Industriële Onderhoudstechnieken
+          aan Sint-Jan Berchmansinstituut vzw, vervolgde ik mijn droom aan de
           Hogeschool Gent met een Graduaat in het Programmeren.
         </p>
       </section>
@@ -51,9 +52,19 @@ const About = () => {
         <p>
           Tijdens mijn studie aan de Hogeschool Gent lag de focus op C#, waar ik
           vaardigheden ontwikkelde in objectgeoriënteerd programmeren en
-          softwareontwikkeling. Mijn interesse in web development groeide ook,
-          wat mij onderscheidt door mijn vermogen om ideeën direct om te zetten
-          in werkende websites, zonder uitgebreid designproces.
+          softwareontwikkeling. Mijn interesse in web development groeide ook
+          aanzienlijk tijdens deze periode. Ik begon te werken met HTML, CSS,
+          JavaScript en ReactJS, waardoor ik de vaardigheden opdeed om
+          interactieve en dynamische websites te creëren.
+        </p>
+        <p>
+          Wat mij onderscheidt is mijn vermogen om ideeën direct om te zetten in
+          werkende websites zonder een uitgebreid designproces. Ik hou ervan om
+          snel te prototypen en itereren op basis van feedback om zo de best
+          mogelijke gebruikerservaring te realiseren. Mijn passie voor front-end
+          development heeft me geholpen om complexe problemen op te lossen en
+          innovatieve oplossingen te bedenken die zowel functioneel als visueel
+          aantrekkelijk zijn.
         </p>
       </section>
     </div>
